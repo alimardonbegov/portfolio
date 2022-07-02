cdca1220defc6a698561025ea13d85bb43fd8473
unknown <alimardon007@gmail.com>
2022-07-02 23:18:27 +0300

    1 commit, for checking connetctions

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshing esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000.");
+});
