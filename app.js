8a03fd3702382dc14009e410099b4b5b02cf0ca1
unknown <alimardon007@gmail.com>
2022-07-10 16:51:54 +0300

    use ejs, add one project, add all links

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("home.ejs");
 });
 
 app.listen(process.env.PORT || 3000, function () {
